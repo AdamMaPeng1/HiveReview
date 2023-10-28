4c55eaf76eca748f1203e4900864d6a2742d7a52
Peng.ma <18852895353@139.com>
2023-10-29 02:51:18 +0800

    1、case when then else end 2、concat(str1,str2);   concat_ws(sep,str1/array<string>,str2);  3.collect_set(),collect_list();  4.later view udtf() tableAlias as columnalias; 5.explode();

--- Hive-知识复习.sql
+++ Hive-知识复习.sql
@@ -88,7 +88,12 @@ max,min,sum,avg,sqrt,
 -- 函数1：nvl()
     空字段赋值: nvl(字段A， 赋值X)
 
--- 函数2： case when then else end
+-- 函数2： case when then else end  -- 可将
+
+-- 行转列：
+    concat(string A/col, string B/col ……) ： 返回输入字符串连接后的结果， 支持任意个输入字符串
+    concat_ws(separator, str1/arrar<array>, str2,..)  : 在每个字符串中添加分隔符进行拼接
+    concat_set(col) : 分组后的聚合函数：函数只接受 基本数据类型，它的主要作用是将某字段的值进行去重汇总，产生 array 类型字段。
 
 */
 -- 1、查看系统自带的函数
@@ -142,21 +147,128 @@ from (
 ) t
 group by dept_id;
 
+-- CASE when then else end 的用法
+   select
+    *,
+    case sex when '男' then 1
+        when '女1' then 2
+        else 0 end as male,
+    case sex when '女' then 1 else 0 end as female
+    from demo.emp_sex
+
+/*行转列：concat() , concat_set()
+    需求：
+       将如下的数据转换成：
+          name	constellation	blood_type
+           孙悟空  白羊座	    A
+           大海   射手座 A
+           宋宋   白羊座 B
+           猪八戒  白羊座 A
+           凤姐	射手座	A
+           苍老师	白羊座	B
+       转换成：
+            射手座,A            大海|凤姐
+            白羊座,A            孙悟空|猪八戒
+            白羊座,B            宋宋|苍老师
+
+  */
+-- 1、创建 表 ： pracCollectSet
+create table if not exists demo.pracCollectSet(
+    name string,
+    constellation string,
+    blood_type string
+)
+row format delimited fields terminated by '\t';
+-- 2、装载数据
+/*需求：
+       将如下的数据转换成：
+          name	constellation	blood_type
+           孙悟空  白羊座	    A
+           大海   射手座 A
+           宋宋   白羊座 B
+           猪八戒  白羊座 A
+           凤姐	射手座	A
+           苍老师	白羊座	B
+       转换成：
+            射手座,A            大海|凤姐
+            白羊座,A            孙悟空|猪八戒
+            白羊座,B            宋宋|苍老师
+  */
+-- 1）先合并 星座+血型
+select
+    name,
+    concat_ws(',', constellation,blood_type)
+from demo.pracCollectSet;
+-- 需求最终结果：2）根据星座+血型的组合字段分组，分组后的name字段进行聚合，使用 collect_set(name)形成去重数组
+select
+    concat_ws('|', collect_set(name)),
+    con
+from (
+    select
+        name,
+        concat_ws(',', constellation,blood_type) con
+    from demo.pracCollectSet
+) t
+group by con;
+
+/*函数：
+  列转行：
+      炸裂：explode(col):将 hive一列中复杂的 array 或者 map 结构拆分成多行
+      侧写：lateral view  udtf(expression) tableAlias as columnAlias
+      split(str, regex) : 通过正则表达式将字符串切分为字符数组
+        如： > SELECT split('oneAtwoBthreeC', '[ABC]') FROM src LIMIT 1;
+               ["one", "two", "three"]
+  需求：
+将如下数据：
+movie	category
+《疑犯追踪》	悬疑,动作,科幻,剧情
+《Lie to me》	悬疑,警匪,动作,心理,剧情
+《战狼2》	战争,动作,灾难
+转换成：
+《疑犯追踪》	悬疑
+《疑犯追踪》	动作
+《疑犯追踪》	科幻
+《疑犯追踪》	剧情
+《Lie to me》	悬疑
+《Lie to me》	警匪
+《Lie to me》	动作
+《Lie to me》	心理
+《Lie to me》	剧情
+《战狼2》	战争
+《战狼2》	动作
+《战狼2》	灾难
+  */
+-- 1、创建表：pracExplode
+create table demo.pracExplode(
+    movie string,
+    category string)
+row format delimited fields terminated by "\t";
+-- 2、将数据加载到表中
+load data local inpath "/home/atguigu/practiceFile/pracExplode.txt" into table demo.pracExplode;
+
+select
+    movie,
+    split(category, ',')[1]
+from demo.pracExplode;
 
 
+select
+    movie,
+    categoryAlias
+from demo.pracExplode
+lateral view
+    explode(split(category, ',')) explodeAlias as categoryAlias;
 
 
+select * from demo.userinfo;
 
-
-
-
-
-
-
-
-
-
-
+-- select
+--     name father,
+--     childrenName,
+--     childrenAge
+-- from demo.userinfo
+-- lateral view
+--     explode(children) userInfoAlias as childrenName,childrenAge ;
 
 
 
